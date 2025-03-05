9ce9e366ceb0ae66cc1c01f019e8c98e4f3ad4bd
Jaca1x <117647243+Jaca7x@users.noreply.github.com>
2025-03-05 14:46:33 -0300

    Update escola.c

--- escola.c
+++ escola.c
@@ -22,193 +22,187 @@ int main() {
     int opcao, opcaoAluno, opcaoProf, cadAluno = 0, cadProf = 0;
     setlocale(LC_ALL, "Portuguese");
 
-    printf("selecione uma opção\n");
+    printf("Selecione uma opÃ§Ã£o\n");
     printf("1 - Alunos\n");
     printf("2 - Professores\n");
     scanf("%d", &opcao);
     
-switch (opcao)
-{
-case 1:
-do {
-    printf("Selecione uma opção\n");
-    printf("1 - Adicionar Aluno\n");
-    printf("2 - Visualizar Alunos\n");
-    printf("3 - Editar Alunos\n");
-    printf("4 - Sair\n");
+    switch (opcao) {
+        case 1:
+            do {
+                printf("Selecione uma opÃ§Ã£o\n");
+                printf("1 - Adicionar Aluno\n");
+                printf("2 - Visualizar Alunos\n");
+                printf("3 - Editar Alunos\n");
+                printf("4 - Sair\n");
 
-    scanf("%d", &opcaoAluno);
+                scanf("%d", &opcaoAluno);
 
-    switch (opcaoAluno) {
-        case 1: {
-            printf("Escreva o nome do aluno: ");
-            scanf("%s", alunos[cadAluno].nome);
-            getchar();
+                switch (opcaoAluno) {
+                    case 1: {
+                        printf("Escreva o nome do aluno: ");
+                        scanf("%s", alunos[cadAluno].nome);
+                        getchar();
 
-            printf("Escreva o número do aluno: ");
-            scanf("%f", &alunos[cadAluno].numero);
-            getchar();
+                        printf("Escreva o nÃºmero do aluno: ");
+                        scanf("%f", &alunos[cadAluno].numero);
+                        getchar();
 
-            printf("Escreva a escolaridade do aluno: ");
-            scanf("%s", alunos[cadAluno].escolaridade);
-            getchar();
+                        printf("Escreva a escolaridade do aluno: ");
+                        scanf("%s", alunos[cadAluno].escolaridade);
+                        getchar();
 
-            cadAluno++;
+                        cadAluno++;
 
-            printf("Aluno adicionado com sucesso!\n");
-            break;
-        }
-
-        case 2: {
-            printf("Alunos do Sistema\n");
-            for (int i = 0; i < cadAluno; i++) {
-                printf("Nome: %s\n", alunos[i].nome);
-                printf("Número: %.2f\n", alunos[i].numero);
-                printf("Escolaridade: %s\n", alunos[i].escolaridade);
-                printf("-------------------------\n");
-            }
-            break;
-        }
-
-        case 3: {
-            if (cadAluno == 0) {
-                printf("Nenhum aluno cadastrado para editar\n");
-                break;
-            }
-
-            char nome[100];
-
-            printf("Escreva o nome do aluno que deseja editar: ");
-            scanf("%s", nome);
-
-            int encontrado = 0;
-
-            for (int i = 0; i < cadAluno; i++) {
-                if (strcmp(alunos[i].nome, nome) == 0) {
-                    printf("Escreva o novo nome do aluno: ");
-                    scanf("%s", alunos[i].nome);
-
-                    printf("Escreva o novo númmero do aluno: ");
-                    scanf("%f", &alunos[i].numero);
-
-                    printf("Escreva a nova escolaridade do aluno: ");
-                    scanf("%s", alunos[i].escolaridade);
-
-                    printf("Aluno editado com sucesso.\n");
-                    encontrado = 1;
-                    break;
-                }
-            }
-
-            if (!encontrado) {
-                printf("Aluno não encontrado\n");
-            }
-
-            break; 
-        }
-
-        case 4: 
-            printf("Encerrando sistema\n");
-            break;
-        default:
-            printf("Opção inválida\n");
-            break;
-    }
-} while (opcaoAluno != 4);
-
-    case 2: {
-        do {
-            printf("Selecione uma opção\n");
-            printf("1 - Adicionar Professor\n");
-            printf("2 - Visualizar Professores\n");
-            printf("3 - Editar Professores\n");
-            printf("4 - Sair\n");
-        
-            scanf("%d", &opcaoProf);
-        
-            switch (opcaoProf) {
-                case 1: {
-                    printf("Escreva o nome do Professor: ");
-                    scanf("%s", professores[cadProf].nome);
-                    getchar();
-        
-                    printf("Escreva o nÃºmero do aluno: ");
-                    scanf("%f", &professores[cadProf].numero);
-                    getchar();
-        
-                    printf("Escreva a escolaridade do aluno: ");
-                    scanf("%s", professores[cadProf].materia);
-                    getchar();
-        
-                    cadProf++;
-        
-                    printf("Professor adicionado com sucesso!\n");
-                    break;
-                }
-        
-                case 2: {
-                    printf("Professores do Sistema\n");
-                    for (int i = 0; i < cadProf; i++) {
-                        printf("Nome: %s\n", professores[i].nome);
-                        printf("Número: %.2f\n", professores[i].numero);
-                        printf("Materia: %s\n", professores[i].materia);
-                        printf("-------------------------\n");
-                    }
-                    break;
-                }
-        
-                case 3: {
-                    if (cadProf == 0) {
-                        printf("Nenhum professor cadastrado para editar\n");
+                        printf("Aluno adicionado com sucesso!\n");
                         break;
                     }
-        
-                    char nome[100];
-        
-                    printf("Escreva o nome do professor que deseja editar: ");
-                    scanf("%s", nome);
-        
-                    int encontrado = 0;
-        
-                    for (int i = 0; i < cadProf; i++) {
-                        if (strcmp( professores[i].nome, nome) == 0) {
-                            printf("Escreva o novo nome do professor: ");
-                            scanf("%s", professores[i].nome);
-        
-                            printf("Escreva o novo númmero do professor: ");
-                            scanf("%f", &professores[i].numero);
-        
-                            printf("Escreva a nova materia do professor: ");
-                            scanf("%s", professores[i].materia);
-        
-                            printf("Aluno editado com sucesso.\n");
-                            encontrado = 1;
+
+                    case 2: {
+                        printf("Alunos do Sistema\n");
+                        for (int i = 0; i < cadAluno; i++) {
+                            printf("Nome: %s\n", alunos[i].nome);
+                            printf("NÃºmero: %.2f\n", alunos[i].numero);
+                            printf("Escolaridade: %s\n", alunos[i].escolaridade);
+                            printf("-------------------------\n");
+                        }
+                        break;
+                    }
+
+                    case 3: {
+                        if (cadAluno == 0) {
+                            printf("Nenhum aluno cadastrado para editar\n");
                             break;
                         }
+
+                        char nome[100];
+
+                        printf("Escreva o nome do aluno que deseja editar: ");
+                        scanf("%s", nome);
+
+                        int encontrado = 0;
+
+                        for (int i = 0; i < cadAluno; i++) {
+                            if (strcmp(alunos[i].nome, nome) == 0) {
+                                printf("Escreva o novo nome do aluno: ");
+                                scanf("%s", alunos[i].nome);
+
+                                printf("Escreva o novo nÃºmero do aluno: ");
+                                scanf("%f", &alunos[i].numero);
+
+                                printf("Escreva a nova escolaridade do aluno: ");
+                                scanf("%s", alunos[i].escolaridade);
+
+                                printf("Aluno editado com sucesso.\n");
+                                encontrado = 1;
+                                break;
+                            }
+                        }
+
+                        if (!encontrado) {
+                            printf("Aluno nÃ£o encontrado\n");
+                        }
+
+                        break; 
                     }
-        
-                    if (!encontrado) {
-                        printf("Professor não encontrado\n");
-                    }
-        
-                    break; 
+
+                    case 4: 
+                        printf("Encerrando sistema\n");
+                        break;
+                    default:
+                        printf("OpÃ§Ã£o invÃ¡lida\n");
+                        break;
                 }
-        
-                case 4: 
-                    printf("Encerrando sistema\n");
-                    break;
-                default:
-                    printf("Opção inválida\n");
-                    break;
-            }
-        } while (opcaoProf != 4);
-        break;
-    }
-    
-   
-} 
+            } while (opcaoAluno != 4);
+            break;
+
+        case 2: {
+            do {
+                printf("Selecione uma opÃ§Ã£o\n");
+                printf("1 - Adicionar Professor\n");
+                printf("2 - Visualizar Professores\n");
+                printf("3 - Editar Professores\n");
+                printf("4 - Sair\n");
+            
+                scanf("%d", &opcaoProf);
+            
+                switch (opcaoProf) {
+                    case 1: {
+                        printf("Escreva o nome do professor: ");
+                        scanf("%s", professores[cadProf].nome);
+                        getchar();
+            
+                        printf("Escreva o nÃºmero do professor: ");
+                        scanf("%f", &professores[cadProf].numero);
+                        getchar();
+            
+                        printf("Escreva a matÃ©ria do professor: ");
+                        scanf("%s", professores[cadProf].materia);
+                        getchar();
+            
+                        cadProf++;
+            
+                        printf("Professor adicionado com sucesso!\n");
+                        break;
+                    }
+            
+                    case 2: {
+                        printf("Professores do Sistema\n");
+                        for (int i = 0; i < cadProf; i++) {
+                            printf("Nome: %s\n", professores[i].nome);
+                            printf("NÃºmero: %.2f\n", professores[i].numero);
+                            printf("MatÃ©ria: %s\n", professores[i].materia);
+                            printf("-------------------------\n");
+                        }
+                        break;
+                    }
+            
+                    case 3: {
+                        if (cadProf == 0) {
+                            printf("Nenhum professor cadastrado para editar\n");
+                            break;
+                        }
+            
+                        char nome[100];
+            
+                        printf("Escreva o nome do professor que deseja editar: ");
+                        scanf("%s", nome);
+            
+                        int encontrado = 0;
+            
+                        for (int i = 0; i < cadProf; i++) {
+                            if (strcmp(professores[i].nome, nome) == 0) {
+                                printf("Escreva o novo nome do professor: ");
+                                scanf("%s", professores[i].nome);
+            
+                                printf("Escreva o novo nÃºmero do professor: ");
+                                scanf("%f", &professores[i].numero);
+            
+                                printf("Escreva a nova matÃ©ria do professor: ");
+                                scanf("%s", professores[i].materia);
+            
+                                printf("Professor editado com sucesso.\n");
+                                encontrado = 1;
+                                break;
+                            }
+                        }
+            
+                        if (!encontrado) {
+                            printf("Professor nÃ£o encontrado\n");
+                        }
+            
+                        break; 
+                    }
+            
+                    case 4: 
+                        printf("Encerrando sistema\n");
+                        break;
+                    default:
+                        printf("OpÃ§Ã£o invÃ¡lida\n");
+                        break;
+                }
+            } while (opcaoProf != 4);
+            break;
+        }
+    } 
 }
-
-
-
-
